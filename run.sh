9bb105b98b7e197aa8d69dcc9e364a9a74366311
Arturo Gonzalez <agonzalezb@eglobal.com.mx>
2018-05-24 17:40:38 -0500

    Se adapta codigo para que los archivos temporales puedan modificarse a nivel global y no estar alambrados

---
+++ run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#If any command fail the script shell exit to immediately
+set -e
+
+########################################################################################################################
+#Remove temp files
+rm init.scala && rm sh/tables_oracle.txt
+
+#Create file temporary init.scala and tables_oracle.txt
+scala_connection="init.scala"
+tables_exported="tables_oracle.txt"
+
+touch ../${scala_connection} && touch ${tables_exported}
+
+
+#Call script in python
+echo "Exportando las tablas de oracle hive"
+python import_oracle_to_hive.py
+
+#######################################################################################################################
+#Call scripts scala and get spark-shell
+echo "Particionando las tablas en hdfs"
+cat scala/header.scala init.scala | ./sh/spark-shell.sh
+
+echo "Eliminado las tablas temporales en hive"
+while IFS= read table_name
+do
+    ./sh/drop_table_hive.sh ${table_name}
+done < sh/tables_oracle.txt
+
+#######################################################################################################################
+#Remove all files temporary
+rm init.scala && rm sh/tables_oracle.txt
+echo "Script finalizado"
+
+
+
+
---
+++ sh/connection.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Version 0.1
+# Author:Arturo Gonzalez Becerril
+
+# *****      DESCRIPTION     *******************************************************************************************
+# This Script  build pool connections to oracle
+# Params
+#  $1 Table name oracle exported
+#  $2 Schema that will contain table hive
+#  $3 Path hdfs will save tables
+#  $4 File that contains scala commands will run into spark-shell
+#  $5
+#
+# Objective: This file is necessary for export data from hive to HDFS.
+
+ if [ $# -ne 5 ]
+   then
+     echo "No se puede construir el fichero init.scala, el numero de parametros es incorrecto"
+     exit 1
+   else
+     table_name_oracle=$1
+     schema_hive=$2
+     path_hdfs_data_partition=$3
+     commands_scala=$4
+     tables_oracle=$5
+
+     echo "println(\" Exportando y particionando la tabla $table_name_oracle a hdfs\")" >> $commands_scala
+     echo "val cliente = hiveContext.sql(\"select * from $schema_hive"."$table_name_oracle\")" >> $commands_scala
+     echo "val dirHDFSCliente:String=\"$path_hdfs_data_partition\"" >> $commands_scala
+     echo "cliente.toDF().selectExpr(\"*\",\"substring(id_mes,0,4) as year\", \"substring(id_mes,5,2) as month\").repartition(10).write.mode(SaveMode.Append).partitionBy(\"year\",\"month\").mode(SaveMode.Append).parquet(\"$path_hdfs_data_partition\")" >> $commands_scala
+     #Add tables oracle will by remove at the end Script
+     echo "$schema_hive"."$table_name_oracle"  >> $tables_oracle
+ fi
